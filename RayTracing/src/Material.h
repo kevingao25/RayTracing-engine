482eb1dc4508b998a2ed0d38f3f4ede9c8899224
Kevin Gao <k27gao@uwaterloo.ca>
2022-04-20 15:40:35 -0400

    Configure camera

--- RayTracing/src/Material.h
+++ RayTracing/src/Material.h
@@ -49,3 +49,43 @@ private:
 	color albedo;
 	double fuzz;
 };
+
+// Dielectrics are materials that refract (water, glass, diamonds)
+class dielectric : public material {
+public:
+	dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+	virtual bool scatter(
+		const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+	) const override {
+		attenuation = color(1.0, 1.0, 1.0);
+		double refraction_ratio = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+		vec3 unit_direction = unit_vector(r_in.direction());
+		double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+		double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+		bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+		vec3 direction;
+
+		if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) {
+			direction = reflect(unit_direction, rec.normal);
+		}
+		else {
+			direction = refract(unit_direction, rec.normal, refraction_ratio);
+		}
+
+		scattered = ray(rec.point, direction);
+		return true;
+	}
+
+private:
+	double refraction_index;
+
+	// Schlick approximation for reflectance
+	static double reflectance(double cosine, double ref_idx) {
+		auto r0 = (1 - ref_idx) / (1 + ref_idx);
+		r0 = r0 * r0;
+		return r0 + (1 - r0) * pow((1 - cosine), 5);
+	}
+};
